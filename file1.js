2242d3136c465df2370ac06984180167a61e29a1
RAHUL KUMAR <95rahulkumarsharma@gmail.com>
2021-06-05 22:49:57 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,57 @@
+// var number1 = 34;
+// var number2 = 54;
+// console.log(number1 + number2);
+// var arr = [1, 2, 3, 4, 5]
+// console.log(arr)
+// var a = 1000;
+// var b = 100;
+// console.log("the value of a + b is", a+b);
+// console.log("the value of a - b is", a-b);
+// console.log("the value of a * b is", a*b);
+// console.log("the value of a / b is", a/b);
+
+// var temp = 0;
+// for(i=0; i<100; i++){
+    // let's divide the value by 2.
+    // if the remainder is zero then it's an even number.
+//     if(i % 2 == 0){
+//     // console.log(i);
+    
+    
+//     if(temp>=i){
+//         temp = i;
+//         console.log(temp)
+//     }
+// }
+//     }
+
+// let highestToLowest = numbers.sort((a, b) => b-a);
+
+
+for(i=100; i>=1; i--){
+    // let's divide the value by 2.
+    // if the remainder is zero then it's an even number.
+    var temp = 1;
+    if(i % 2 == 0){
+        
+        if(i>=temp){
+			temp=i;
+			
+		}
+		console.log(i);
+}
+    }
+	
+
+for(i=1; i<=100; i++){
+    // let's divide the value by 2.
+    // if the remainder is zero then it's an even number.
+    var temp = 1;
+    if(i % 2 == 0){
+        if(i>=temp){
+			temp=i;
+			
+		}
+		console.log(i);
+}
+    }
---
+++ file2.js
@@ -0,0 +1,2 @@
+const array = [5, 10, 15, 20, 25, 30];
+console.log(array.indexOf(30));
